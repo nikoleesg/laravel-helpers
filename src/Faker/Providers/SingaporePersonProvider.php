68d82e03282052e7cc3925aa878d5eb20037af60
Niko Lee <niko.lee@outlook.sg>
2026-03-18 10:15:15 +0800

    feat: Vastly expand SingaporePersonProvider with more Chinese identities and mapped English given names

--- src/Faker/Providers/SingaporePersonProvider.php
+++ src/Faker/Providers/SingaporePersonProvider.php
@@ -8,7 +8,10 @@ class SingaporePersonProvider extends Person
 {
     protected static $lastName = [
         'Tan', 'Lim', 'Lee', 'Ng', 'Ong', 'Wong', 'Goh', 'Chua', 'Chan', 'Koh',
-        'Teo', 'Ang', 'Poh', 'Neo', 'Sim', 'Chong', 'Chia', 'Yeo', 'Tay', 'Low'
+        'Teo', 'Ang', 'Poh', 'Neo', 'Sim', 'Chong', 'Chia', 'Yeo', 'Tay', 'Low',
+        'Toh', 'Choo', 'Chee', 'Cheong', 'Chew', 'Chin', 'Chow', 'Foo', 'Gan',
+        'Heng', 'Ho', 'Khoo', 'Lau', 'Loh', 'Loo', 'Lum', 'Phua', 'Seah', 'Song',
+        'Quek', 'Teng', 'Ting', 'Wee', 'Yap', 'Yong', 'Aw', 'Boon', 'Cheng'
     ];
 
     protected static $firstNameMale = [
@@ -21,15 +24,50 @@ class SingaporePersonProvider extends Person
         'Ai', 'Yu', 'Shu', 'Qing', 'Na', 'Xia', 'Yun', 'Zhen', 'Ling', 'Xiu', 'Qun'
     ];
 
+    protected static $firstNameMaleEn = [
+        'Alex', 'Henry', 'Ethan', 'Noah', 'Lucas', 'Oliver', 'Liam', 'Jayden', 'Isaac', 'Benjamin', 
+        'Caleb', 'Daniel', 'Joshua', 'Jacob', 'John', 'Jonathan', 'Josiah', 'Jovan', 'Justin',
+        'Ayden', 'Kayden', 'Eden', 'Hayden', 'Shawn', 'Shaun', 'Sean', 'Vincent', 'Desmond',
+        'Alvin', 'Kelvin', 'Melvin', 'Bryan', 'Ryan', 'Joel', 'Leon', 'Marcus', 'Gideon'
+    ];
+
+    protected static $firstNameFemaleEn = [
+        'Julyn', 'Michelle', 'Sarah', 'Ashley', 'Olivia', 'Chloe', 'Sophia', 'Emma', 'Charlotte', 
+        'Amelia', 'Harper', 'Evelyn', 'Haley', 'Hailey', 'Hana', 'Hannah', 'Hazel', 'Heather', 
+        'Heidi', 'Hope', 'Amanda', 'Melissa', 'Rachel', 'Jessica', 'Nicole', 'Jasmine', 'Shirley', 
+        'Grace', 'Joanne', 'Felicia', 'Charmaine', 'Eunice', 'Valerie', 'Bernice', 'Serene'
+    ];
+
     protected static $formats = [
         '{{lastName}} {{firstName}}',
+        '{{firstNameEn}} {{lastName}}',
+        '{{firstNameEn}} {{lastName}} {{firstName}}',
     ];
 
     protected static $maleNameFormats = [
         '{{lastName}} {{firstNameMale}}',
+        '{{firstNameMaleEn}} {{lastName}}',
+        '{{firstNameMaleEn}} {{lastName}} {{firstNameMale}}',
     ];
 
     protected static $femaleNameFormats = [
         '{{lastName}} {{firstNameFemale}}',
+        '{{firstNameFemaleEn}} {{lastName}}',
+        '{{firstNameFemaleEn}} {{lastName}} {{firstNameFemale}}',
     ];
+
+    public function firstNameMaleEn()
+    {
+        return static::randomElement(static::$firstNameMaleEn);
+    }
+
+    public function firstNameFemaleEn()
+    {
+        return static::randomElement(static::$firstNameFemaleEn);
+    }
+
+    public function firstNameEn()
+    {
+        return static::randomElement(array_merge(static::$firstNameMaleEn, static::$firstNameFemaleEn));
+    }
 }
